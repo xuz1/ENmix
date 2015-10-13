35029f098cd253605504cd6dd8e9b069cd5cced4
Zongli Xu <xuz@niehs.nih.gov>
2015-10-13 20:40:43 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 88f85db7b3743683d1b637d4f7463f7ea4425f6a
    
        commit11
    
        Signed-off-by: Zongli Xu <xuz@niehs.nih.gov>
        
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ENmix@109593 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bmiq.mc.R
+++ R/bmiq.mc.R
@@ -17,13 +17,25 @@ bmiq.mc <-function(mdat,nCores=1,...)
     design.v[design.v == "II"]=2 
     design.v <- as.numeric(design.v)
 
-    c1 <- makeCluster(nCores)
-    registerDoParallel(c1)
-    beta.o <- foreach (s = 1:ncol(beta.b),.combine=cbind,.export=c("BMIQ"))  %dopar%{
-    s=s;out <- BMIQ(beta.b[, s], design.v, plots = FALSE,sampleID = colnames(beta.b)[s],...)
-        return(out$nbeta)
+    if(.Platform$OS.type == "unix"){
+        BMIQ.b <-function(ID,beta.b,design.v,...)
+        {
+            out<-BMIQ(beta.b[,ID], design.v, plots = FALSE,...)
+            out$nbeta
+        }
+        beta.o=mclapply(1:ncol(beta.b),BMIQ.b,beta.b=beta.b,design.v=design.v,...,
+               mc.silent=TRUE,mc.cores=nCores)
+        beta.o <- do.call(cbind, lapply(beta.o, unlist))
+    }else{
+        c1 <- makeCluster(nCores)
+        registerDoParallel(c1)
+        beta.o <- foreach (s = 1:ncol(beta.b),.combine=cbind,.export=c("BMIQ"))  %dopar%{
+        s=s;out <- BMIQ(beta.b[, s], design.v=design.v, plots = FALSE,...)
+            return(out$nbeta)
+        }
+        stopCluster(c1)
     }
-    stopCluster(c1)
     colnames(beta.o) <- colnames(beta.b)
     beta.o
 }
+
